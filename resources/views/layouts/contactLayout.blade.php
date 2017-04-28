134c4ba92f615e81dd81a36c4fba70a13312ed20
Nitesh <nitesh.chandel61@gmail.com>
2017-04-28 09:19:41 +0530

    Name added in header

--- resources/views/layouts/contactLayout.blade.php
+++ resources/views/layouts/contactLayout.blade.php
@@ -39,6 +39,11 @@
             </div>
             <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li>
+                        <a href="/contacts">Contact List Management System</a>
+                    </li>
+                </ul>
                 <ul class="nav navbar-nav navbar-right">
                     <li>
                         <a href="/contacts">Contacts</a>
